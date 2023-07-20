a196c1c13d590843d5c15c1be6349d8346b0ae4c
LettyMoila <moilankibelo@gmail.com>
2023-07-20 15:55:32 +0200

    added cloud storage for images

--- src/layouts/BookBtn.jsx
+++ src/layouts/BookBtn.jsx
@@ -1,7 +1,7 @@
 const BookBtn = ({text}) =>{
     return(
         <div className="p-2">
-            <button className="bg-buttonBack w-36 h-10 rounded-full">{text}</button>
+            <button onClick className="bg-buttonBack w-36 h-10 rounded-full">{text}</button>
         </div>
     )
 }
--- src/pages/ManageRooms.jsx
+++ src/pages/ManageRooms.jsx
@@ -2,63 +2,54 @@ import { useState } from 'react'
 import {db} from '../config/firebase'
 import { collection, addDoc } from 'firebase/firestore'
 
-import Wait from '../assets/Wait.png'
-import View from '../assets/View.png'
-import Tick from '../assets/Tick Box.png'
-import Cancel from '../assets/Cancel.png'
+import { imgOB } from '../config/firebase'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
 import { Link } from 'react-router-dom'
+import TopBar from '../layouts/TopBar'
+import BookBtn from '../layouts/BookBtn'
 
 const ManageRooms = () => {
+    const [discount, setDiscount] = useState('');
+    const [roomType, setRoom] = useState('');
+    const [beds, setBed] = useState('');
     const [price, setPrice] = useState('');
+    const [pict, setImg] = useState('');
 
     const add = (async() =>{
  
     try{
         const docRef = await addDoc(collection(db, 'rooms'),{
+            discount: discount,
+            roomType: roomType,
+            beds: beds,
             price: price,
+            pict: pict,
             });
+            const imgRef = ref(imgOB, `rooms/${imageUpload.name}`);
+
+            uploadBytes(imgRef, imageUpload).then((snapshot) => {
+                getDownloadURL(snapshot.ref).then((url) => {
+                    console.log(url);
+                });
+            })
             alert("Added successfully")
         }catch(error){
 
         }
-    
-
-
 })
     
     return(
         <div className="h-screen">
-            <section className="h-1/6 bg-buttonBack flex justify-between">
-                <div>
-                <span className="material-symbols-outlined justify-middle">menu</span>
-                </div>
-                <div className='flex gap-8 list-none'>
-                    <li className=''>
-                        <img src={Wait} />
-                        <label className='flex align-middle'>pending</label>
-                    </li>
-                    <li>
-                    <img src={Tick}/>
-                    <label>Approved</label>
-                    </li>
-                    <li>
-                    <img src={Cancel}/>
-                    <label>Canceled</label>
-                    </li>
-                    <li>
-                    <img src={View}/>
-                    <label>View history</label>
-                    </li>
-                </div>
-                <div>
-                    <Link to="/AdminLogin">
-                    <span className="material-symbols-outlined">logout</span>
-                    </Link>
-                </div>
-            </section>
-            <section className="h-5/6 bg-vintageColor">
-                <div>
-                    <input type='text' onChange={(e) =>setPrice(e.target.value)}></input>
+            <TopBar/>
+            <section className="h-5/6 bg-vintageColor p-6">
+                <div className='grid grid-cols-6 gap-5'>
+                    <input type='text' placeholder='Discount' onChange={(e) =>setDiscount(e.target.value)}/>
+                    <input type='text' placeholder='room type' onChange={(e) =>setRoom(e.target.value)}/>
+                    <input type='text' placeholder='bed number and type/s' onChange={(e) =>setBed(e.target.value)}/>
+                    <input type='text' placeholder='Price' onChange={(e) =>setPrice(e.target.value)}/>
+                    <input type='file' onChange={(e) =>setImg(e.target.files[0])}/>
+                    <BookBtn onClick={add} text={"Done"}/>
                 </div>
             </section>
         </div>
